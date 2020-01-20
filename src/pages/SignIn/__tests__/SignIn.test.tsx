e19be3a28e715724ab654e0ba9a7eb2f4e6ce8a5
LougieQ <lougie.quisel@gmail.com>
2020-01-20 11:28:47 +0800

    Apply testing for the toggle component

--- src/pages/SignIn/__tests__/SignIn.test.tsx
+++ src/pages/SignIn/__tests__/SignIn.test.tsx
@@ -3,28 +3,33 @@ import { mount, ReactWrapper } from 'enzyme';
 import Root from '../../../Root';
 import SignIn from '../SignIn';
 import SignInForm from '../SignInForm';
+import Toggle from 'components/Toggle/Toggle';
 
 describe('SignIn component', () => {
-	let wrapper: ReactWrapper;
+  let wrapper: ReactWrapper;
 
-	beforeEach(() => {
-		wrapper = mount(
-			<Root>
-				<SignIn />
-			</Root>
-		);
-	});
+  beforeEach(() => {
+    wrapper = mount(
+      <Root>
+        <SignIn />
+      </Root>
+    );
+  });
 
-	afterEach(() => {
-		wrapper.unmount();
-	});
+  afterEach(() => {
+    wrapper.unmount();
+  });
 
-	it('shows the username & password', () => {
-		expect(wrapper.find('[data-test="username-txt"]')).toHaveLength(1);
-		expect(wrapper.find('[data-test="password-txt"]')).toHaveLength(1);
-	});
+  it('shows the username & password', () => {
+    expect(wrapper.find('[data-test="username-txt"]')).toHaveLength(1);
+    expect(wrapper.find('[data-test="password-txt"]')).toHaveLength(1);
+  });
 
-	it('shows the sign in form', () => {
-		expect(wrapper.find(SignInForm)).toHaveLength(1);
-	});
+  it('shows the sign in form', () => {
+    expect(wrapper.find(SignInForm)).toHaveLength(1);
+  });
+
+  it('shows the Toggle component', () => {
+    expect(wrapper.find(Toggle)).toHaveLength(1);
+  });
 });
