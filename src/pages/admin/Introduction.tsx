66ca67e402ce2b9c7a1ed63eee2e0b5be2ab1b78
LougieQ <lougie.quisel@gmail.com>
2020-01-21 12:34:48 +0800

    Remove unused component

--- src/pages/admin/Introduction.tsx
+++ src/pages/admin/Introduction.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, ButtonToolbar, Button } from 'react-bootstrap';
+import { Jumbotron, Button } from 'react-bootstrap';
 
 const Introduction = () => {
   return (
