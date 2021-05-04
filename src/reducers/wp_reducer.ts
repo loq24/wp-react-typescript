e073629cfe8045bb04ba3bf2c07564089b5b5ff7
LougieQ <lougie.quisel@gmail.com>
2021-05-04 20:52:30 +0800

    Fix import/no-anonymous-default-export issue

--- src/reducers/wp_reducer.ts
+++ src/reducers/wp_reducer.ts
@@ -1,51 +1,53 @@
 import {
-	FetchCurrentUserAction,
-	User,
-	ActionWPTypes,
-	FetchPostsAction,
-	FetchPostAction,
-	UpdatePostAction,
-	PublishPostAction,
-	DeletePostAction,
-	TestingWPAction,
-	Post
-} from '../actions';
+  FetchCurrentUserAction,
+  User,
+  ActionWPTypes,
+  FetchPostsAction,
+  FetchPostAction,
+  UpdatePostAction,
+  PublishPostAction,
+  DeletePostAction,
+  TestingWPAction,
+  Post
+} from "../actions";
 
 type Actions =
-	| FetchCurrentUserAction
-	| FetchPostsAction
-	| FetchPostAction
-	| UpdatePostAction
-	| PublishPostAction
-	| DeletePostAction
-	| TestingWPAction;
+  | FetchCurrentUserAction
+  | FetchPostsAction
+  | FetchPostAction
+  | UpdatePostAction
+  | PublishPostAction
+  | DeletePostAction
+  | TestingWPAction;
 
 export interface InitialState {
-	currentUser?: User;
-	posts: Post[];
-	post?: Post;
+  currentUser?: User;
+  posts: Post[];
+  post?: Post;
 }
 
 export const initialState: InitialState = {
-	posts: []
+  posts: []
 };
 
-export default function(state = initialState, action: Actions) {
-	switch (action.type) {
-		case ActionWPTypes.fetchCurrentUser:
-			return { ...state, currentUser: action.payload };
-		case ActionWPTypes.fetchPosts:
-			return { ...state, posts: action.payload, post: undefined };
-		case ActionWPTypes.updatePost || ActionWPTypes.publishPost:
-			return { ...state, posts: [] };
-		case ActionWPTypes.fetchPost:
-			return { ...state, post: action.payload };
-		case ActionWPTypes.deletePost:
-			return {
-				...state,
-				posts: state.posts.filter((post: Post) => post.id !== action.payload)
-			};
-		default:
-			return state;
-	}
-}
+const wpReducer = (state = initialState, action: Actions) => {
+  switch (action.type) {
+    case ActionWPTypes.fetchCurrentUser:
+      return { ...state, currentUser: action.payload };
+    case ActionWPTypes.fetchPosts:
+      return { ...state, posts: action.payload, post: undefined };
+    case ActionWPTypes.updatePost || ActionWPTypes.publishPost:
+      return { ...state, posts: [] };
+    case ActionWPTypes.fetchPost:
+      return { ...state, post: action.payload };
+    case ActionWPTypes.deletePost:
+      return {
+        ...state,
+        posts: state.posts.filter((post: Post) => post.id !== action.payload)
+      };
+    default:
+      return state;
+  }
+};
+
+export default wpReducer;
