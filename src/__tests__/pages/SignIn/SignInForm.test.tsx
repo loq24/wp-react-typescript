ddc8210b0f9fdb6cba97f396fb72a9235018b8bc
LougieQ <lougie.quisel@gmail.com>
2021-05-04 22:02:54 +0800

    Fix failing tests

--- src/__tests__/pages/SignIn/SignInForm.test.tsx
+++ src/__tests__/pages/SignIn/SignInForm.test.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { mount, ReactWrapper } from 'enzyme';
-import Root from 'Root';
-import SignInForm from 'pages/SignIn/SignInForm';
-import { initialFormValues } from 'utils/helpers';
+import React from "react";
+import { mount, ReactWrapper } from "enzyme";
+import Root from "Root";
+import SignInForm from "pages/SignIn/SignInForm";
+import { initialFormValues } from "utils/helpers";
+import { waitForComponentToPaint } from "utils/mock_helpers";
 
-describe('SignIn Form Component', () => {
+describe("SignIn Form Component", () => {
   let wrapper: ReactWrapper;
   beforeEach(() => {
     wrapper = mount(
@@ -17,11 +18,11 @@ describe('SignIn Form Component', () => {
     wrapper.unmount();
   });
 
-  it('shows warning message when the input is wrong', () => {
+  it("shows warning message when the input is wrong", () => {
     wrapper.unmount();
     const mockMsg = {
       msg: {
-        warning: 'Username or password is incorrect.'
+        warning: "Username or password is incorrect."
       }
     };
     wrapper = mount(
@@ -32,11 +33,11 @@ describe('SignIn Form Component', () => {
     expect(wrapper.find('div[data-test="warning-msg"]')).toHaveLength(1);
   });
 
-  it('shows a message after logging out', () => {
+  it("shows a message after logging out", () => {
     wrapper.unmount();
     const mockMsg = {
       msg: {
-        success: 'Thanks for trying the app.'
+        success: "Thanks for trying the app."
       }
     };
     wrapper = mount(
@@ -47,66 +48,71 @@ describe('SignIn Form Component', () => {
     expect(wrapper.find('div[data-test="ty-msg"]')).toHaveLength(1);
   });
 
-  it('successfully submits', () => {
+  it("successfully submits", async () => {
     expect(wrapper.find('span[data-test="submitting"]')).toHaveLength(0);
-    wrapper.find('form').simulate('submit', {
+    wrapper.find("form").simulate("submit", {
       preventDefault: () => {}
     });
+
+    await waitForComponentToPaint(wrapper);
 
     expect(wrapper.find('span[data-test="submitting"]')).toHaveLength(1);
     expect(wrapper.find('button[type="submit"]').props().disabled).toBeTruthy();
   });
 
-  describe('Username Field', () => {
+  describe("Username Field", () => {
     let usernameField: any;
     let fieldName: string;
     beforeEach(() => {
-      fieldName = 'username';
+      fieldName = "username";
       usernameField = wrapper.find(`input[name="${fieldName}"]`);
     });
 
-    it('has a correct default username value', () => {
+    it("has a correct default username value", () => {
       expect(usernameField.props().value).toEqual(initialFormValues.username);
     });
 
-    it('should update an input when it is changed', () => {
-      const testFieldValue = 'testusernamevalue';
-      usernameField.simulate('change', {
+    it("should update an input when it is changed", async () => {
+      const testFieldValue = "testusernamevalue";
+      usernameField.simulate("change", {
         persist: () => {},
         target: {
           name: fieldName,
           value: testFieldValue
         }
       });
-      wrapper.update();
+
+      await waitForComponentToPaint(wrapper);
+
       expect(wrapper.find(`input[name="${fieldName}"]`).props().value).toEqual(
         testFieldValue
       );
     });
   });
 
-  describe('Password Field', () => {
+  describe("Password Field", () => {
     let passwordField: any;
     let fieldName: string;
     beforeEach(() => {
-      fieldName = 'password';
+      fieldName = "password";
       passwordField = wrapper.find(`input[name="${fieldName}"]`);
     });
 
-    it('it has a correct default password value', () => {
+    it("it has a correct default password value", () => {
       expect(passwordField.props().value).toEqual(initialFormValues.password);
     });
 
-    it('should update an input when it is changed', () => {
-      const testFieldValue = 'testpasswordvalue';
-      passwordField.simulate('change', {
+    it("should update an input when it is changed", async () => {
+      const testFieldValue = "testpasswordvalue";
+      passwordField.simulate("change", {
         persist: () => {},
         target: {
           name: fieldName,
           value: testFieldValue
         }
       });
-      wrapper.update();
+      await waitForComponentToPaint(wrapper);
+
       expect(wrapper.find(`input[name="${fieldName}"]`).props().value).toEqual(
         testFieldValue
       );
